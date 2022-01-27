77f2275648563b9688c9ea0d62033836df754b2d
kristenomahony <95743303+kristenomahony@users.noreply.github.com>
2022-01-26 20:16:49 -0800

    Update search.js
    sorry forgot

--- controllers/search.js
+++ controllers/search.js
@@ -4,7 +4,6 @@ const router = express.Router()
 
 // Views
 router.get('/', (req, res) => {
-  res.send('This is the search page')
 })
 // Create here a controller that accepts GET requests and renders the "search" page
 
